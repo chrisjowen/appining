9deca398a8735799888c9bda39ee8b3664634fa8
Chris Owen <chris.j.owen@hotmail.co.uk>
2016-09-29 14:08:31 +0100

    Added meetup client file

--- api/spec/appining/integration/scrapers/meetup/meetup_client_spec.exs
+++ api/spec/appining/integration/scrapers/meetup/meetup_client_spec.exs
@@ -2,15 +2,15 @@ defmodule Appining.Scrapers.Meetup.MeetupClientSpec do
   use ESpec
   alias Appining.Scrapers.Meetup.MeetupClient
 
-  context "Can retrieve meetup event search data", only: :integration do
+  context "Can retrieve meetup event search data", context_tag: :integration  do
     let :lat, do: 123
     let :long, do: 123
     let :radius, do: 123
     let :page, do: 1
 
-    it "should parse correctly" do
+    it "should parse correctly"  do
       result = MeetupClient.search(lat, long, radius, page)
-      result |> should_not(eq nil) 
+      result |> should_not(eq nil)
     end
   end
 
